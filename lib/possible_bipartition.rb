b3c232b9edc72a37683d726614eea4cde57933a4
Rinostar <codingrinostar@gmail.com>
2020-05-17 14:20:51 -0700

    bipartition

--- lib/possible_bipartition.rb
+++ lib/possible_bipartition.rb
@@ -1,4 +1,18 @@
-
 def possible_bipartition(dislikes)
-  raise NotImplementedError, "possible_bipartition isn't implemented yet"
+  groups = []
+  
+  dislikes.length.times do |i|
+    if groups[i].nil?
+      groups[i] = 1
+    end
+
+    if !dislikes[i].nil?
+      dislikes[i].each do |n|
+        groups[n] = (-groups[i]) if groups[n].nil?
+        return false if groups[n] == groups[i]
+      end
+    end
+  end
+
+  return true
 end
